fc3d2668d1a770c688ef1417c9e808d8b9c4f78f
jasoneminyard <jason.e.minyard@gmail.com>
2024-06-26 13:55:40 -0500

    lift state up

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -53,6 +53,23 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
   );
 }
 
